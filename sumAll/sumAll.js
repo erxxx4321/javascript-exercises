0386a40dda4e0c0ccf4024144eac6f3ec585d807
Yu Fang Chin <>
2021-06-30 12:13:29 +0800

    removeFromArray reverseString sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+	let sum = 0;
+	if (start < end) {
+		for (let i = start; i <= end; i++) {
+			sum += i;
+		}
+	}
 
+	if (start > end) {
+		for (let i = start; i >= end; i--) {
+			sum += i;
+		}
+	}
+
+	if (sum < 0) {
+		return "ERROR";
+	}
+	if (!Number.isInteger(start) || !Number.isInteger(end)) {
+		return "ERROR";
+	}
+	return sum;
 };
 
 module.exports = sumAll;

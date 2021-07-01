68378b59ba10a2fcc794ebb8491f671aaae5fb1f
Yu Fang Chin <>
2021-07-01 13:09:42 +0800

    getTheTitles helloWorld palindromes repeatString

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	str = str.replace(/\s/g, "").toLowerCase();
+	let newStr = "";
+	let reversedStr = "";
+	var punctuations = "`~!@#$%^&*()_+{}|:\"<>?-=[];'./,";
 
+	// filter the string content
+	for (let i = 0; i < str.length; i++) {
+		if (punctuations.includes(str[i])) {
+			newStr += "";
+		} else {
+			newStr += str[i];
+		}
+	}
+
+	for (let j = newStr.length - 1; j >= 0; j--) {
+		reversedStr += newStr[j];
+	}
+
+	if (newStr === reversedStr) return true;
+	else return false;
 };
 
 module.exports = palindromes;
+
+console.log(palindromes("A car, a man, a maraca."));
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (str, times) {
+	if (times < 0) {
+		return "ERROR";
+	}
 
+	let count = 0;
+	let newStr = "";
+	while (count < times) {
+		for (let i = 0; i < str.length; i++) {
+			newStr += str[i];
+		}
+		count++;
+	}
+	return newStr;
 };
 
 module.exports = repeatString;

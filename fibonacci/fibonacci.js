b2971fea392d1f209cd091c7b537060f4dc03492
Yu Fang Chin <>
2021-07-04 14:11:53 +0800

    calculator fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+	let a = 0;
+	let b = 1;
+	let show = 0;
 
+	for (let i = 0; i < Number(num) - 1; i++) {
+		show = a + b;
+		a = b;
+		b = show;
+	}
+
+	if (Number(num) < 0) return "OOPS";
+	else if (Number(num) === 0) return a;
+	else if (Number(num) === 1) return b;
+	else return show;
 };
 
 module.exports = fibonacci;
